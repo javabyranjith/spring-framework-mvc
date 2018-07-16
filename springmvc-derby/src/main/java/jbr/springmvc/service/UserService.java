ffab8dc8c560881db9cd46d831f336116e79bf9d
Ranjith Sekar <java2ranjith@gmail.com>
2018-07-16 23:03:26 +0530

    update

---
+++ springmvc-derby/src/main/java/jbr/springmvc/service/UserService.java
@@ -0,0 +1,10 @@
+package jbr.springmvc.service;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public interface UserService {
+	void register(User user);
+
+	User validateUser(Login login);
+}
