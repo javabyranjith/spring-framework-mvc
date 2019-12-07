98c92e291df203bf571db9e9cf1bcc07205aa213
Ranjith Sekar <java2ranjith@gmail.com>
2019-12-07 16:52:28 +0530

    gitignore added

---
+++ spring-mvc-embedded-tomcat/README.md
@@ -0,0 +1,6 @@
+## TOOLS & TECHNOLOGIES
+1. Java 1.8
+2. Embedded Tomcat
+
+## CONCEPTS COVERED
+1. JdbcTemplate
--- spring-mvc-embedded-tomcat/src/main/java/jbr/springmvc/gradle/HelloController.java
+++ spring-mvc-embedded-tomcat/src/main/java/jbr/springmvc/gradle/HelloController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hello")
-	public String hello(ModelMap map) {
-		map.put("message", "HELLO. Welcome to Spring MVC using GRADLE!!!");
-		return "hello";
-	}
+  @RequestMapping("/hello")
+  public String hello(ModelMap map) {
+    map.put("message", "HELLO. Welcome to Spring MVC using Embedded Tomcat!!!");
+    return "hello";
+  }
 
-	@RequestMapping("/bye")
-	public String bye(ModelMap map) {
-		map.put("message", "GOOD BYE. Meet you soon!!!");
-		return "bye";
-	}
+  @RequestMapping("/bye")
+  public String bye(ModelMap map) {
+    map.put("message", "GOOD BYE. Meet you soon!!!");
+    return "bye";
+  }
 }
